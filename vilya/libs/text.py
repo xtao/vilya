ff4a66db6a3548dc7db2c76947a6c275fc3ccab9
XTao <xutao881001@gmail.com>
2014-08-22 00:00:35 +0800

    Introduce mikoto & ellen.

---
+++ vilya/libs/text.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import docutils
+from mikoto import Mikoto
+
+# FIXME new api
+from mikoto.libs.text import RST_RE
+
+def format_md_or_rst(path, content, project_name=None):
+    m = Mikoto(content)
+    if path.endswith('.md') or path.endswith('.markdown'):
+        # FIXME with project or gist
+        return m.markdown
+
+    if RST_RE.match(path):
+        try:
+            return m.restructuredtext
+        except docutils.ApplicationError:
+            pass
+
+    return m.code
---
+++ vilya/projects/repository.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os
+from ..libs.git import Repository
+from ..libs.text import format_md_or_rst
+from ..settings import REPO_PATH
+
+REPO_ROOT_PATH = REPO_PATH
+
+
+class ProjectRepository(Repository):
+
+    def __init__(self, project):
+        path = os.path.join(REPO_ROOT_PATH, project.path)
+        self.project = project
+        super(ProjectRepository, self).__init__(path)
+
+    def get_commit(self, reference):
+        pass
+
+    def get_readme(self, reference='HEAD', path=None):
+        entries = self.repository.list_entries(reference=reference,
+                                               path=path)
+        for path in entries:
+            entry = entries[path]
+            if (entry.is_blob and entry.name == 'README' or entry.name.startswith('README.')):
+                readme_content = self.get_file(reference, path)
+                return format_md_or_rst(path, readme_content)
+        return ''
+
+    def get_rendered_file(self, reference='HEAD', path=None):
+        content = self.get_file(reference=reference, path=path)
+        if content:
+            return format_md_or_rst(path, content)
